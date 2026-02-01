aa5b19de6ef7bfba1f7874208d1be90cd40cd8f9
Patricia <patyfb04@gmail.com>
2026-02-01 17:30:51 -0500

    remove unecessary comment

--- Program.cs
+++ Program.cs
@@ -12,7 +12,6 @@ builder.Services.AddDbContext<TripContext>(options =>
     options.UseSqlServer(conn));
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCors();
